24cf4d94ad3acdd092e59460ac6b0ad3bb813e8e
Larissa <lariqlopes@gmail.com>
2022-06-23 19:11:28 -0400

    index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { recupJaulaDeZelador } from './controllers/JaulaController'
 import { recupEspecimeDeEspecie, recupEspecimeDaJaula, recupEspecimeDeZelador } from './controllers/EspecimeController'
 import { recupZeladorDeEspecieme } from './controllers/ZeladorController'
 
-
 const run = async () => {
   await createTables()
   await clearData()
@@ -13,6 +12,27 @@ const run = async () => {
   /**
    * Execute as funções responsáveis pelas consultas aqui!
    */
+
+  const habitatDaEspecie = await recupEspeciePeloHabitat('mata atlântica brasileira')
+  console.log(habitatDaEspecie)
+
+  const nomeCientificoDaEspecie = await recupEspeciePeloNomeCientifico('Leontopithecus rosalia')
+  console.log(nomeCientificoDaEspecie)
+
+  const especimeDaEspecie = await recupEspecimeDeEspecie(nomeCientificoDaEspecie)
+  console.log(especimeDaEspecie)
+
+  const jaulaDaEspecime = await recupEspecimeDaJaula('00009')
+  console.log(jaulaDaEspecime)
+
+  const especimeZelador = await recupEspecimeDeZelador('Pedro Martinez Bezerra')
+  console.log(especimeZelador)
+
+  const jaulaZelador = await recupJaulaDeZelador('Pedro Martinez Bezerra')
+  console.log(jaulaZelador)
+
+  const zeladores = await recupZeladorDeEspecieme('12002')
+  console.log(zeladores)
 
   /**
    * Fim das consultas
